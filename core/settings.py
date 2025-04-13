ca3afc2fb5c628f86cf3918c41860294fc9db29e
Lucas Meira <lucasrenaa1996@gmail.com>
2025-04-13 17:43:51 +0000

    test password fake

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,8 @@ username_token = "admin"
 password_token = "admin"
 username_token2 = "admin"
 password_token2 = "admin"
-
+username_token3 = "admin"
+password_token3 = "admin"
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
